37dcbe819a9ddc1bba91821c7fccec223cd2cc0c
Bianca Monteiro <bianca_souza_monteiro@hotmail.com>
2019-10-18 16:06:33 -0300

    commit de projeto vuejs

---
+++ cadastrodeusuarios/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+
+  <router-view>  <login @do-sign-in="doSignIn"/></router-view>
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+    components: {
+
+    },
+    methods: {
+      doSignIn(obj) {
+      window.console.log(obj);
+    }
+    }
+}
+</script>
+
---
+++ cadastrodeusuarios/src/views/Signout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Signout!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
