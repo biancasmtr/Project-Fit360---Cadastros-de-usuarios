e181317ca4d3b3a02d8be7db4f6680494d9e92d4
Bianca Monteiro <bianca_souza_monteiro@hotmail.com>
2019-10-18 15:15:12 -0300

    cadastro de usuarios com vuejs

---
+++ cadusuarios/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+
+  <router-view></router-view>
+
+</template>
+
+<script>
+ import Login from './views/Login'
+
+export default {
+  name: 'app',
+  components: {
+    Login,
+  },
+  methods: {
+
+  }
+}
+</script>
+
---
+++ cadusuarios/src/views/Home.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Primeiro</th>
+            <th scope="col">Último</th>
+            <th scope="col">Nickname</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <button type="button" class="btn btn-primary">
+        <router-link to="login">  Logout</router-link>
+      </button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "home",
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+  .table {
+    width: 70%;
+    margin: 0 auto;
+    margin-top: 5%;
+  }
+</style>
