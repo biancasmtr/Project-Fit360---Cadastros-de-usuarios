3524b902d6c6fc3f44c15542e6829b331044f97b
Bianca Monteiro <b.monteiro@fingerprint.com.br>
2019-11-01 16:04:09 -0300

    commit de projeto

---
+++ Cadastro-leve/resources/js/components/Painel.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <div class="card">
+        <div class="card-header">
+            {{titulo}}
+        </div>
+
+        <div class="card-body">
+
+            <slot></slot>
+
+        </div>
+    </div>
+
+
+</template>
+
+<script>
+export default {
+    props: ['titulo','cor']
+}
+</script>
