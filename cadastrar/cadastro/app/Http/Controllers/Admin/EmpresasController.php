d58f5fc74fbedcd0b9689a36cd98deda184de784
Bianca Monteiro <bianca_souza_monteiro@hotmail.com>
2019-11-01 16:22:17 -0300

    commit de projeto

---
+++ cadastrar/cadastro/app/Http/Controllers/Admin/EmpresasController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Empresa;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class EmpresasController extends Controller
+{
+
+    protected $redirectTo = '/register';
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $listaMigalhas = json_encode([
+            ["titulo"=>"Home","url"=>route('home')],
+            ["titulo"=>"Lista de Usuários","url"=>""]
+        ]);
+
+        $listaModelo = Empresa::all('id_empresa','razao_social','endereco','cnpj','telefone');
+
+        return view('admin.empresas.index',compact('listaMigalhas','listaModelo'));
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'razao_soacial' => ['required', 'string', 'max:255'],
+            'endereco' => ['string', 'email', 'max:255'],
+            'cnpj' => ['required', 'string', 'min:8', 'unique:users']
+        ]);
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+}
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        dd($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Empresa::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+        $validacao = \Validator::make($data, [
+          
+        ]);
+
+        if($validacao->fails()){
+            return redirect()->back()->withErrors($validacao)->withInput();
+        }
+
+        User::find($id)->update($data);
+        return redirect()->back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Empresa::find($id)->delete();
+        return redirect()->back();
+    }
+}
---
+++ cadastrar/cadastro/app/Http/Controllers/HomeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Empresa;
+use App\User;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $listaMigalhas = json_encode([
+            ["titulo"=>"Home","url"=>""]
+        ]);
+
+            $totalUsuarios = User::count();
+            $totalEmpresas = Empresa::count();
+
+        return view('home',compact('listaMigalhas','totalUsuarios','totalEmpresas'));
+    }
+}
