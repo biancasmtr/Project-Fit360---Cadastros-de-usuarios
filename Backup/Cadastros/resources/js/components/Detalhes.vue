f4cd1059f64779f028c694c56660872c304f5f84
Bianca Monteiro <bianca_souza_monteiro@hotmail.com>
2019-11-27 16:21:57 -0200

    cadastro de usuários e empresas com Vuejs + laravel + mysql + API ReceitaWS

---
+++ Backup/Cadastros/resources/js/components/Detalhes.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <table class="table table-striped table-hover">
+      <tbody>
+        <tr v-for="item in lista" :key="item">
+          <td v-for="i in item" :key="i">{{i}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+        }
+    },
+    
+    computed: {
+        lista: function() {
+            return this.itens;
+        }
+    },
+    props:['itens','url'],
+}
+</script>
+
